a0bdb8f1848d8ec1c26b0abd8ca68b3eb88fd311
Cain Norris <cmn+github@cainnorris.net>
2010-03-14 17:31:08 -0400

    sidentd: commandline arg, daemonizing, priv dropping

--- Sidentd.hs
+++ Sidentd.hs
@@ -3,28 +3,33 @@ module Main (main) where
 import Network
 import System.IO
 import System.Timeout
+import System (getArgs, exitFailure)
 import Control.Monad (forever)
 import Control.Concurrent (forkIO)
 import Control.Exception (finally)
 import System.Posix (setGroupID, setUserID)
-
-user = "cain"   -- the static username returned for all queries
+import System.Posix.Daemonize (daemonize)
+import Data.Char (isPrint)
 
 main :: IO ()
-main = withSocketsDo $ do
-  s <- listenOn $ PortNumber 113
-  setGroupID 1
-  setUserID 1
-  forever $ do
-    (h, host, port) <- accept s
-    forkIO (handleConnection h)
+main = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "usage: sidentd USER" >> exitFailure
+    (user:_) -> do
+      s <- listenOn $ PortNumber 113
+      setGroupID 1
+      setUserID 1
+      daemonize $ forever $ do
+        (h, host, port) <- accept s
+        forkIO (handleConnection user h)
 
-handleConnection :: Handle -> IO ()  -- this is the control flow of each connection thread
-handleConnection h = finally hndlr cleanup where
+handleConnection :: String -> Handle -> IO ()  -- this is the control flow of each connection thread
+handleConnection user h = finally hndlr cleanup where
   hndlr = withTimeout $ do
     hSetBuffering h LineBuffering
     query <- hGetLine h
-    hPutStr h (query ++ " : USERID : UNIX : " ++ user)
+    hPutStrLn h ((filter isPrint query) ++ " : USERID : UNIX : " ++ user)
   cleanup = hClose h
   withTimeout a = do
     r <- timeout (30*(10^6)) a
