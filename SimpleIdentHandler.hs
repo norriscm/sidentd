d030857bcf1fc37e5f2e676ff2c301e3e5b53bce
Cain Norris <cmn+github@cainnorris.net>
2010-03-14 14:37:38 -0400

    add readme

--- SimpleIdentHandler.hs
+++ SimpleIdentHandler.hs
@@ -1,7 +1,10 @@
 module SimpleIdentHandler (handleQuery) where
 
+user :: String -- the static user
+user = "cain"
+
 handleQuery :: Int -> Int -> IO (String, String) -- do the lookup
-handleQuery lport fport = return ("USERID","UNIX : cain")
+handleQuery lport fport = return ("USERID","UNIX : " ++ user)
 
 
 
